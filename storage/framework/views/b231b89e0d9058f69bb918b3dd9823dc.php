92bbfbf61d87242d4b32765e3654400f87cc20d7
TanakaSenpai <sakifshahedur7@gmail.com>
2025-07-14 23:15:58 +0600

    Refactor cache clearing route for improved readability

--- storage/framework/views/b231b89e0d9058f69bb918b3dd9823dc.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b><?php echo e(__('common.Version')); ?></b> 3.0.3
-    </div>
-    <strong><?php echo e(__('common.Copyright')); ?>Copyright &copy; 2014-<?php echo e(date("Y")); ?> <a href="https://hostvelly.com" target="_blank">HOSTVELLY.COM</a>.</strong> <?php echo e(__('common.all_reserved')); ?>
-
-  </footer><?php /**PATH D:\Shahin Files + Live Projects\LIVE PROJECTS\aone-pos-laravel\aone.qoyelxyz.com\resources\views\user\layouts\partials\footer.blade.php ENDPATH**/ ?>
